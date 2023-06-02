1a68f710705349f8ddf9a16dab2dac7e9c25d335
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2023-06-02 22:27:25 +0800

    add the ndjson and csv demo

--- demo.py
+++ demo.py
@@ -52,4 +52,44 @@ class jsonDemo(AbstractDemo):
             json.dump(self.output_data, file, indent=4)
 
 
+class ndjsonDemo(AbstractDemo):
+    def __init__(self, file_name: str, file_extension: str, number_of_object: int = 3) -> None:
+        super().__init__(file_name, file_extension, number_of_object)
+
+    def load_file(self, file_name, file_extension):
+        print("Loading ndjson file: " + file_name + "." + file_extension)
+        with open(self.input_directory + file_name + "." + file_extension, "r") as file:
+            return file.readlines()
+
+    def create_demo(self):
+        print("Creating demo for ndjson file")
+        count = 0
+        while count < self.number_of_object:
+            self.output_data.append(self.file[count])
+            count += 1
+
+        with open(self.output_directory + self.file_name + "_demo" + "." + self.file_extension, "w") as file:
+            file.writelines(self.output_data)
+
+
+class csvDemo(AbstractDemo):
+    def __init__(self, file_name: str, file_extension: str, number_of_object: int = 3) -> None:
+        super().__init__(file_name, file_extension, number_of_object)
+
+    def load_file(self, file_name, file_extension):
+        print("Loading csv file: " + file_name + "." + file_extension)
+        with open(self.input_directory + file_name + "." + file_extension, "r") as file:
+            return file.readlines()
+
+    def create_demo(self):
+        print("Creating demo for csv file")
+        count = 0
+        while count < self.number_of_object + 1:  # the first line is the header
+            self.output_data.append(self.file[count])
+            count += 1
+
+        with open(self.output_directory + self.file_name + "_demo" + "." + self.file_extension, "w") as file:
+            file.writelines(self.output_data)
+
+
 eval(file_extension + "Demo")(file_name, file_extension, number_of_object)
