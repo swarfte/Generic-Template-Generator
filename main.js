056bf627ae14845900e6a6006b35b18c1821f754
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2023-06-02 21:03:12 +0800

    fix the logic error of the init env

--- main.js
+++ main.js
@@ -1,51 +1,36 @@
 const fs = require("fs");
 
-function generateTemplate() {
+function generateTemplate(folderList) {
     // used for generating a template
-    if (
-        // check if the template, source and output directories exist
-        fs.existsSync("./template") &&
-        fs.existsSync("./source") &&
-        fs.existsSync("./output")
-    ) {
-        const templateName = process.argv.slice(2)[0]; // the first argument is the template name (without .js)
-        let generatorName = process.argv.slice(2)[1]; // the second argument is the generator format name (such as json, ndjson, csv)
+    const templateName = process.argv.slice(2)[0]; // the first argument is the template name (without .js)
+    let generatorName = process.argv.slice(2)[1]; // the second argument is the generator format name (such as json, ndjson, csv)
 
-        if (generatorName === undefined) {
-            // the default generator format is json
-            generatorName = "json";
-        }
-
-        const Generator = require("./tool/generator.js")[
-            generatorName + "Generator"
-        ];
-
-        new Generator(templateName).run();
+    if (generatorName === undefined) {
+        // the default generator format is json
+        generatorName = "json";
     }
+
+    const Generator = require("./tool/generator.js")[
+        generatorName + "Generator"
+    ];
+
+    new Generator(templateName).run();
 }
 
-function initializeEnvironment() {
+function initializeEnvironment(folderList) {
+    let isInitialized = true;
     // used for create the directories if they don't exist
-    if (!fs.existsSync("./template")) {
-        // create the template directory if it doesn't exist
-        fs.mkdirSync("./template");
-        return false;
+    for (const folder of folderList) {
+        if (!fs.existsSync("./" + folder)) {
+            fs.mkdirSync("./" + folder);
+            isInitialized = false;
+        }
     }
 
-    if (!fs.existsSync("./source")) {
-        // create the source directory if it doesn't exist
-        fs.mkdirSync("./source");
-        return false;
-    }
-
-    if (!fs.existsSync("./output")) {
-        // create the output directory if it doesn't exist
-        fs.mkdirSync("./output");
-        return false;
-    }
-    return true;
+    return isInitialized;
 }
 
-if (initializeEnvironment()) {
+folderList = ["template", "source", "output", "demo"];
+if (initializeEnvironment(folderList)) {
     generateTemplate();
 }
